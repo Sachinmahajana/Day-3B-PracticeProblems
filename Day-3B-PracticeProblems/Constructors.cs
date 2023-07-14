5568c4159b97856863b00b83535402bab3ad2b44
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-14 10:16:07 +0530

    [Sachin] Adding Constructor

---
+++ Day-3B-PracticeProblems/Constructors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3B_PracticeProblems
+{
+    internal class Constructors
+    {
+        public int a;
+        public int b;
+        // This is Un Parameterrised Constructor
+        public Constructors()
+        {
+            Console.WriteLine("This Is Parameter Less Constructor");
+        }
+        // This Is Parameter rised Constructor
+        public Constructors(int a, int b)
+        {
+            int Sum = a + b;
+            Console.WriteLine(Sum);
+        }
+    }
+}
--- Day-3B-PracticeProblems/Program.cs
+++ Day-3B-PracticeProblems/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Day 3b Practice Problems");
+            Console.WriteLine("\n1:Constructor");
+            Console.WriteLine("Enter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    new Constructors();
+                    new Constructors(10, 20);
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
