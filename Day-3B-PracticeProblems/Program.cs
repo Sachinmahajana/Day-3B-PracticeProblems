b8589b541a21ef5edde236453c2f03468b585949
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-14 10:21:44 +0530

    [Sachin] Adding Constructor

--- Day-3B-PracticeProblems/Program.cs
+++ Day-3B-PracticeProblems/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Day 3b Practice Problems");
-            Console.WriteLine("\n1:Constructor");
+            Console.WriteLine("\n1:Constructor\n2:AcessModifier");
             Console.WriteLine("Enter A Option");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch (Option)
@@ -13,6 +13,11 @@
                 case 1:
                     new Constructors();
                     new Constructors(10, 20);
+                    break;
+                case 2:
+                    AcessModifier acc = new AcessModifier();
+                    acc.Numbers(10, 20);
+                    acc.Numb(50, 60);
                     break;
             }
             Console.ReadLine();
