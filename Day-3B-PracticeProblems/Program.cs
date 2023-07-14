265e2b5dd3b5e15c5d5f62f64eb87c2e4ddecb4f
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-14 10:09:24 +0530

    [Sachin] Adding Welcome Message

---
+++ Day-3B-PracticeProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_3B_PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Day 3b Practice Problems");
+        }
+    }
+}
